43de7a2b328e5dce5f7176cf221ce3b35225a021
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2024-03-05 18:06:08 +0000

    [squash] bin ranges build bucket from account name env var

--- bin-ranges-transfer/src/main/java/uk/gov/pay/java_lambdas/bin_ranges_transfer/config/Constants.java
+++ bin-ranges-transfer/src/main/java/uk/gov/pay/java_lambdas/bin_ranges_transfer/config/Constants.java
@@ -3,15 +3,18 @@ package uk.gov.pay.java_lambdas.bin_ranges_transfer.config;
 import software.amazon.awssdk.regions.Region;
 import uk.gov.pay.java_lambdas.bin_ranges_transfer.model.Version;
 
+import static java.lang.String.format;
+
 public class Constants {
 
     private Constants() {
     }
 
     public static final Region AWS_REGION = Region.of(System.getenv("AWS_REGION"));
+    public static final String AWS_ACCOUNT_NAME = System.getenv("AWS_ACCOUNT_NAME");
     public static final String SFTP_DIRECTORY = "/Streamline/Universal";
     public static final String SFTP_FILE_PREFIX = "WP_341BIN_";
-    public static final String S3_BUCKET_NAME = System.getenv("S3_BUCKET_NAME");
+    public static final String S3_BUCKET_NAME = format("bin-ranges-staging-%s", AWS_ACCOUNT_NAME);
     public static final String PASSPHRASE_PARAMETER_NAME = System.getenv("PASSPHRASE_PARAMETER_NAME");
     public static final String PRIVATE_KEY_PARAMETER_NAME = System.getenv("PRIVATE_KEY_PARAMETER_NAME");
     public static final Version WORLDPAY_FILE_VERSION = Version.fromEnvironment(System.getenv("WORLDPAY_FILE_VERSION"));
