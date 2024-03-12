8187b25ae0e30b49d34391223f64b8234bb7ba60
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2024-03-12 16:34:32 +0000

    Starter for integrity check.

--- bin-ranges-integrity/src/main/java/uk/gov/pay/java_lambdas/bin_ranges_integrity/App.java
+++ bin-ranges-integrity/src/main/java/uk/gov/pay/java_lambdas/bin_ranges_integrity/App.java
@@ -1,18 +1,80 @@
 package uk.gov.pay.java_lambdas.bin_ranges_integrity;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import uk.gov.pay.java_lambdas.bin_ranges_integrity.util.RequestBuilder;
+import uk.gov.pay.java_lambdas.common.bin_ranges.dto.Candidate;
 
-public class App {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
+
+import static uk.gov.pay.java_lambdas.bin_ranges_integrity.config.Constants.*;
+
+public class App implements RequestHandler<Candidate, Candidate> {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+    private static final Base64.Encoder BASE_64_ENCODER = Base64.getEncoder();
+    private final S3Client s3Client;
 
-    public static void main(String... args)
-    {
-        logger.info("Lambda starts");
+    public App() {
+        s3Client = DependencyFactory.s3Client();
+    }
 
-        Handler handler = new Handler();
-        handler.sendRequest();
+    @Override
+    public Candidate handleRequest(Candidate candidate, final Context context) {
+        logger.info("fn: {}, version: {}.", context.getFunctionName(), context.getFunctionVersion());
+        List<String> linesFromS3File = getLinesFromS3File(S3_STAGING_BUCKET_NAME, candidate.s3Key());
+        if (linesFromS3File == null) {
+            logger.error("Failed to read file contents {}", candidate.s3Key());
+            return result(candidate, false); 
+        }
+        List<String> linesFromS3PromotedFile = getLinesFromS3File(S3_PROMOTED_BUCKET_NAME, PROMOTED_BIN_RANGES_S3_KEY);
+        if (linesFromS3PromotedFile == null) {
+            logger.error("Failed to read promoted file contents {}", PROMOTED_BIN_RANGES_S3_KEY);
+            return result(candidate, false);
+        }
 
-        logger.info("Lambda ends");
+        int candidateLineCount = linesFromS3File.size();
+        int promotedLineCount = linesFromS3PromotedFile.size();
+        
+        if (candidateLineCount > Math.floorDiv(promotedLineCount * (100 + ACCEPTABLE_LINE_DIFFERENCE_PERCENTAGE), 100)) {
+            logger.error("Contents of candidate is too large {}, candidate is {} lines long, promoted is {} lines long", candidate.s3Key(), candidateLineCount, promotedLineCount);
+            return result(candidate, false);
+        }
+        
+        if (candidateLineCount < Math.ceilDiv(promotedLineCount * (100 - ACCEPTABLE_LINE_DIFFERENCE_PERCENTAGE), 100)) {
+            logger.error("Contents of candidate is too short {}, candidate is {} lines long, promoted is {} lines long", candidate.s3Key(), candidateLineCount, promotedLineCount);
+            return result(candidate, false);
+        }
+        return result(candidate, true);
+    }
+
+    private static Candidate result(Candidate candidate, boolean shouldProceed) {
+        return Candidate.from(candidate, shouldProceed);
+    }
+
+
+    private List<String> getLinesFromS3File(String s3StagingBucketName, String candidateKey) {
+        // todo: check for pre-existing sha digests on the promoted head object
+
+        var getCandidateBinRangesRequest = RequestBuilder.getObjectRequest(s3StagingBucketName, candidateKey);
+
+        try (ResponseInputStream<GetObjectResponse> s3ObjectInputStream = s3Client.getObject(getCandidateBinRangesRequest);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(s3ObjectInputStream, StandardCharsets.UTF_8))) {
+
+            logger.debug("Reading file {}", candidateKey);
+            return reader.lines().toList();
+        } catch (IOException e) {
+            logger.error("Error reading file from S3: {}", e.getMessage());
+        }
+        return null;
     }
 }
--- bin-ranges-integrity/src/test/java/uk/gov/pay/java_lambdas/bin_ranges_integrity/AppTest.java
+++ bin-ranges-integrity/src/test/java/uk/gov/pay/java_lambdas/bin_ranges_integrity/AppTest.java
@@ -1,16 +1,91 @@
 package uk.gov.pay.java_lambdas.bin_ranges_integrity;
 
+
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import uk.gov.pay.java_lambdas.bin_ranges_integrity.config.Constants;
+import uk.gov.pay.java_lambdas.common.bin_ranges.dto.Candidate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AppTest {
-    /**
-     * Rigorous Test :-)
-     * Mock s3 etc
-     */
-    @Test
-    void shouldAnswerWithTrue() {
-        assertTrue(true);
+    @Mock
+    private Context context;
+    @Mock
+    private S3Client mockS3Client;
+    private MockedStatic<Constants> constantsMockedStatic;
+    private MockedStatic<DependencyFactory> dependencyFactoryMockedStatic;
+
+    @BeforeEach
+    public void setUp() {
+        constantsMockedStatic = mockStatic(Constants.class);
+        dependencyFactoryMockedStatic = mockStatic(DependencyFactory.class);
+        dependencyFactoryMockedStatic.when(DependencyFactory::s3Client).thenReturn(mockS3Client);
+        when(context.getFunctionName()).thenReturn(this.getClass().getName());
+        when(context.getFunctionVersion()).thenReturn("TEST");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        constantsMockedStatic.close();
+        dependencyFactoryMockedStatic.close();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "baseline,true", 
+        "tiny,false",
+        "acceptably-large,true",
+        "acceptably-small,true",
+        "just-too-large,false",
+        "just-too-small,false"
+    })
+    void handleRequest_baselineShouldProceed(String input, boolean expected) throws IOException {
+        String baselineDataPath = Paths.get("src/test/resources/testData/candidate_baseline.csv").toString();
+        String candidateDataPath = Paths.get("src/test/resources/testData", format("candidate_%s.csv", input)).toString();
+
+        try (FileInputStream baselineFIS = new FileInputStream(baselineDataPath);
+        FileInputStream candidateFIS = new FileInputStream(candidateDataPath)) {
+
+            ResponseInputStream<GetObjectResponse> baselineResponseInputStream = new ResponseInputStream<>(GetObjectResponse.builder().build(), baselineFIS);
+            ResponseInputStream<GetObjectResponse> candidateResponseInputStream = new ResponseInputStream<>(GetObjectResponse.builder().build(), candidateFIS); 
+
+            when(mockS3Client.getObject(any(GetObjectRequest.class)))
+                .thenReturn(candidateResponseInputStream)
+                .thenReturn(baselineResponseInputStream);
+
+            App function = new App();
+            Candidate candidate = new Candidate("/an/s3/key.csv", true);
+            Candidate result = function.handleRequest(candidate, context);
+            assertEquals(expected, result.proceed());
+
+            verify(mockS3Client, atMost(0)).putObject(any(PutObjectRequest.class), any(RequestBody.class));
+        }
     }
 }
