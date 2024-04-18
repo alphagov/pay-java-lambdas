7d1332cb637cc24bfc8bc20d8f69cd087e0e40a7
Nathaniel Steers <nathaniel.steers@digital.cabinet-office.gov.uk>
2024-04-18 09:57:16 +0100

    [squash] get absolute value of percentage change

--- bin-ranges-integrity/src/main/java/uk/gov/pay/java_lambdas/bin_ranges_integrity/util/PercentageChange.java
+++ bin-ranges-integrity/src/main/java/uk/gov/pay/java_lambdas/bin_ranges_integrity/util/PercentageChange.java
@@ -6,6 +6,6 @@ public class PercentageChange {
     }
     public static double get(long candidate, long promoted) {
         if (promoted == 0) throw new IllegalArgumentException("Promoted value cannot be 0");
-        return ((double) (candidate - promoted) / promoted) * 100;
+        return Math.abs((double) (candidate - promoted) / promoted) * 100;
     }
 }
